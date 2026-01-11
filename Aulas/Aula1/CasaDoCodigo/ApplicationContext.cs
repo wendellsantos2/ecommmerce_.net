a10fa4d7409ce86cca5e8903e9e380841a1e18b9
wendellsantos2 <wemdellcos@gmail.com>
2026-01-11 16:25:40 -0300

    feat : projeto inicial

---
+++ Aulas/Aula1/CasaDoCodigo/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CasaDoCodigo
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected ApplicationContext()
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Produto>().HasKey(t => t.Id);
+        }
+    }
+}
